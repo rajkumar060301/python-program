efdb258164e502852ff0fbe4d45d36abdca62634
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2022-03-06 23:29:31 +0530

    initial commit

---
+++ introduction/largestArray.py
@@ -0,0 +1,7 @@
+list = []
+for i in range(1, 5 + 1):
+    element = int(input())
+    list.append(element)
+anotherArray = max(list)
+print("my list: ", list)
+print("largest element: ", anotherArray)
